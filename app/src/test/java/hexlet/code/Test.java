f93be00503ba70894f79d3210a0558bd863391c2
annkolesnikova <annkolesnikova@ozon.ru>
2024-11-08 17:20:58 +0300

    init

---
+++ app/src/test/java/hexlet/code/Test.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InitTest {
+    @Test
+    void initTest() {
+        Assertions.assertEquals(4, 2 * 2);
+    }
+}
